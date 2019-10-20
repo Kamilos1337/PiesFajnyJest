e250f52e4900c52a58a5e780579438c3d7bd9a28
Kamilos1337 <kamilzachradnik1337@gmail.com>
2019-10-20 14:43:23 +0200

    Test olek

--- src/client/components/contact.js
+++ src/client/components/contact.js
@@ -14,10 +14,11 @@ export default class User extends React.Component {
 }
      render(props){
        return(
-         <div className="Diego">
+         <div className="">
            <Container>
-             <Row className="contactRow">
+             <Row className="contactRow status">
                <Col className="contactCOL">
+                Jestem olek
                 <img className="contactIMG" src={Contact} alt="Skontaktuj się z piesfajnyjest.com"/>
                   <FormGroup className="styleInput marginTop20">
                    <Input type="text" className="inputBor" name="contactTitle" id="contactTitle" placeholder="Podaj temat..." />
