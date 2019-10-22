144eea6654b4a3ca717ff345b390cc74e84305f5
Kamilos1337 <kamilzachradnik1337@gmail.com>
2019-10-22 17:14:30 +0200

    Pierwszy commit

--- src/client/components/contact.js
+++ src/client/components/contact.js
@@ -18,7 +18,7 @@ export default class User extends React.Component {
            <Container>
              <Row className="contactRow status">
                <Col className="contactCOL">
-                Jestem olek
+
                 <img className="contactIMG" src={Contact} alt="Skontaktuj się z piesfajnyjest.com"/>
                   <FormGroup className="styleInput marginTop20">
                    <Input type="text" className="inputBor" name="contactTitle" id="contactTitle" placeholder="Podaj temat..." />
